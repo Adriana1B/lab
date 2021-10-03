280bda48ca29cb21e09f4bdc817f324917f239ce
Adriana1B <91206918+Adriana1B@users.noreply.github.com>
2021-10-03 14:18:16 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,50 +4,48 @@ using namespace std;
 int main()
 {
 	double x;
-	double y;
-	double A;
-	double B;
-	cout << "x= "; cin >> x;
-	A = 1 + 9 * x;
+	double b;
+	double c;
+	double a;
+	double F;
+	cout << "x = "; cin >> x;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "a = "; cin >> a;
 
-	if (x<=0)
+	//1 розгал.повн
+
+	if (x<0 && b!=0)
 	{
-		B= log(abs(sin(x))) + pow(x, 7);
+		F = a * pow(x, 2) + b;
 	}
-	if (x <= 3 && x > 0)
+	if (x>0 && b==0)
 	{
-		B = 1 / tan((abs(x + 1)) / 2);
+		F = x - a / x - c;
 	}
-	if (x<3)
+	if (!(x < 0 && b != 0) && !(x > 0 && b == 0))
 	{
-		B = 3 * x - pow(x, 5);
+		F = x / c;
 	}
-	y = A + B;
-	y = 1 + 9 * x;
 	cout << endl;
-	cout << "1) y = " << y << endl;
-	//2v
-	if (x <= 0)
+	cout << "1) F = " << F << endl;
+//2скор.
+	if (x < 0 && b != 0)
 	{
-		B = log(abs(sin(x))) + pow(x, 7);
+		F = a * pow(x, 2) + b;
 	}
 	else
 	{
-		if (x<=3 && x>0)
+		if (x > 0 && b == 0)
 		{
-			B = 1 / tan((abs(x + 1))) / 2;
+			F = x - a / x - c;
 		}
 		else
 		{
-			if (x<3)
-			{
-				B = 3 * x - pow(x, 5);
-			}
+			F = x / c;
 		}
-		y = A + B;
-		y = 1 + 9 * x;
-		cout << "2) y = " << y << endl;
-		return 0;
 	}
-	
+	cout << "2) F = " << F << endl;
+	cin.get();
+	return 0;
 }
